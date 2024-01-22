0b628950ce421b9a59710701edcd902068e74959
Semyon Gritsenko <40473939+LazyPanda07@users.noreply.github.com>
2024-01-22 23:53:34 +0500

    Refactor

--- Tests/src/Main.cpp
+++ Tests/src/Main.cpp
@@ -1,9 +1,5 @@
 #include "gtest/gtest.h"
 
-#pragma comment(lib, "gtest.lib")
-#pragma comment(lib, "gtest_main.lib")
-#pragma comment(lib, "ThreadPool.lib")
-
 int main(int argc, char** argv)
 {
 	testing::InitGoogleTest(&argc, argv);
